ebdf764181722f2c806daabf5056afb353164019
Steven Anaya <steven.anaya@cs.nmt.edu>
2020-01-23 00:40:05 -0700

    Implement token label pretty printing and improve scanning

--- include/Scanner.hpp
+++ include/Scanner.hpp
@@ -9,21 +9,32 @@ class Scanner {
 public:
     enum TokenLabel {
         Identifier,
+        Keyword,
+        TypeIdentifier,
         StringLiteral,
         NumberLiteral,
         CharacterLiteral,
-        SpecialSymbol,
-        Keyword,
-        TypeIdentifier,
+        Comment,
         Operator,
+        SpecialSymbol,
+        PreprocessorDirective,
+        Invalid
     };
+
     typedef std::pair<std::string, TokenLabel> Token;
 
     Scanner();
     std::vector<Token> tokenize(std::string source);
-    int capturedGroups(std::smatch& match);
+    TokenLabel identifyToken(std::smatch& match);
+    const char *tokenLabelToString(TokenLabel tokenLabel);
 
 private:
+    const std::vector<const char*> tokenLabels {
+        "Identifier", "Keyword", "TypeIdentifier", "StringLiteral",
+        "NumberLiteral", "CharacterLiteral", "Comment", "Operator",
+        "SpecialSymbol", "PreprocessorDirective", "Invalid"
+    };
+
     const std::set<std::string> keywords{
         "case", 	"enum",
         "register", "typedef",
@@ -32,8 +43,9 @@ private:
         "char", 	"extern",
         "return", 	"union",
         "do", 	    "if",
-        "static",   "while",
-        "struct"
+        "static",   "switch",
+        "for",      "while",
+        "struct",   "extern"
     };
 
     const std::set<std::string> typeIdentifiers{
@@ -50,16 +62,17 @@ private:
         "{", "}", "[", "]", "(", ")", ";", ","
     };
 
-    int groups = 7; // XXX REMEMBER TO CHANGE IF YOU F*CK WITH THE REGEX
+    const int regex_groups = 9; // Remember to change if you modify the regex
     const std::regex pattern = std::regex(  
-        "([a-zA-Z_$][a-zA-Z_$\\d]*)"//Identifiers, keywords
-        "|(\".*\")"                 //String literals
-        "|(\\d+)"                   //Number literals
-        "|('.')"                    //Character literals
-        "|([=()*{}&,;>+\\[\\].])"   //Operators, symbols
-        "|(#[a-zA-Z_]+)"            //Pre-processor directives
-        "|(\\/\\/.*)"               //In-line comments
-        "|(\\/\\*[.\n]*\\*\\/)"     //Block comments
-        "|(#include *<.*>)"         //Include statements
-        );
+        "([a-zA-Z_][a-zA-Z_\\d]*)"               // Identifiers, keywords
+        "|(\".*\")"                              // String literals
+        "|(-?\\d+\\.?\\d*)"                      // Number literals
+        "|('.')"                                 // Character literals
+        "|(\\/\\/.*)"                            // In-line comments
+        "|(\\/\\*[\\S\\s]*\\*\\/)"               // Block comments
+        "|(<<=?|>>=?|\\+\\+|--|&&|\\|\\||\\.|->" // Operators
+            "|[-+*/=<>!%&\\|^]=?)"               // More operators
+        "|([(){}\\[\\],;])"                   // Special symbols
+        "|(#[a-zA-Z_]+ .*)"                      // Pre-processor directives
+    );
 };
--- src/Scanner.cpp
+++ src/Scanner.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <Scanner.hpp>
 
-Scanner::Scanner() {
-
-}
+Scanner::Scanner() {}
 
 std::vector<Scanner::Token> Scanner::tokenize(const std::string source)
 {
@@ -11,19 +9,13 @@ std::vector<Scanner::Token> Scanner::tokenize(const std::string source)
     try {
         std::sregex_iterator next(source.begin(), source.end(), pattern);
         std::sregex_iterator end;
-        while (next != end) {
+        for (; next != end; next++) {
             std::smatch match = *next;
-            std::cout << "[" << match.str(0) << "], "
-                // << " , " << match.str(1)
-                // << " , " << match.str(2)
-                // << " , " << match.str(3)
-                // << " , " << match.str(4)
-                << " , " << match.str(100);
-                // << std::endl;
-            tokens.emplace_back(match.str(0), TokenLabel::Identifier);
-            next++;
+            std::string token = match.str();
+            TokenLabel tokenLabel = identifyToken(match);
+            std::cout << "[" << token << ", " << tokenLabelToString(tokenLabel) << "]," << std::endl;
+            tokens.emplace_back(token, tokenLabel);
         }
-        std::cout << std::endl;
     } catch (std::regex_error& e) {
         // Syntax error in the regular expression
         printf("Regex syntax error\n");
@@ -32,13 +24,51 @@ std::vector<Scanner::Token> Scanner::tokenize(const std::string source)
     return tokens;
 }
 
-int Scanner::capturedGroups(std::smatch& match)
+Scanner::TokenLabel Scanner::identifyToken(std::smatch& match)
 {
-    for (int i = 0; i < this->groups; i++) {
+    for (int i = 1; i <= this->regex_groups; i++) {
         if (!match.str(i).empty()) {
-            return i;
+            switch (i) {
+                // 1 Identifiers, keywords, type identifiers
+                case 1:
+                    if (keywords.find(match.str(i)) != keywords.end()) {
+                        return TokenLabel::Keyword;
+                    } else if (typeIdentifiers.find(match.str(i)) != typeIdentifiers.end()) {
+                        return TokenLabel::TypeIdentifier;
+                    } else {
+                        return TokenLabel::Identifier;
+                    }
+                // 2 String literals
+                case 2:
+                    return TokenLabel::StringLiteral;
+                // 3 Number literals
+                case 3:
+                    return TokenLabel::NumberLiteral;
+                // 4 Character literals
+                case 4:
+                    return TokenLabel::CharacterLiteral;
+                // 5 In-line comments
+                case 5:
+                // 6 Block comments
+                case 6:
+                    return TokenLabel::Comment;
+                // 7 Operators
+                case 7:
+                    return TokenLabel::Operator;
+                // 8 Special symbols
+                case 8:
+                    return TokenLabel::SpecialSymbol;
+                // 9 Pre-processor directives
+                case 9:
+                    return TokenLabel::PreprocessorDirective;
+            }
         }
     }
 
-    return -1;
+    return TokenLabel::Invalid;
+}
+
+const char *Scanner::tokenLabelToString(TokenLabel tokenLabel)
+{
+    return tokenLabels.at(tokenLabel);
 }
