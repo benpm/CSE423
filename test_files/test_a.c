d7f877f7dfd6e986c54dad59734ed4ad7f5c06c3
Steven Anaya <steven.anaya@cs.nmt.edu>
2020-01-23 00:41:55 -0700

    Improve testing

--- test_files/test_a.c
+++ test_files/test_a.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    int value = 0;
+    int value = -5;
     //int 0vaije;
-    int he$k;
+    int hek = 5.4 * -18.;
     /*
     boi
     */
     char a = '	';
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < TEST; i++) {
         if (i % 10 == 5) {
             value += i;
         }
