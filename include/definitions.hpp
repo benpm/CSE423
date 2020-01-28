71061d9f30ab1de113a7292119633bb2417f49f9
Steven Anaya <steven.anaya@cs.nmt.edu>
2020-01-28 09:41:18 -0700

    Refactor to Google namespace style

--- include/definitions.hpp
+++ include/definitions.hpp
@@ -3,12 +3,13 @@
 #include <string>
 #include <set>
 
-namespace definitions
-{
-    extern const std::set<std::string> keywords;
-    extern const std::set<std::string> typeIdentifiers;
-    extern const std::set<std::string> unaryOperators;
-    extern const std::set<std::string> binaryOperators;
-    extern const std::set<std::string> ternaryOperators;
-    extern const std::set<std::string> specialSymbols;
+namespace definitions {
+
+extern const std::set<std::string> keywords;
+extern const std::set<std::string> typeIdentifiers;
+extern const std::set<std::string> unaryOperators;
+extern const std::set<std::string> binaryOperators;
+extern const std::set<std::string> ternaryOperators;
+extern const std::set<std::string> specialSymbols;
+
 }
--- src/definitions.cpp
+++ src/definitions.cpp
@@ -2,45 +2,47 @@
 
 namespace definitions
 {
-    //Valid C keywords
-    const std::set<std::string> keywords{
-        "case",     "enum",
-        "register", "typedef",
-        "default",  "goto",
-        "sizeof",   "volatile",
-        "char",     "extern",
-        "return",   "union",
-        "do",       "if",
-        "static",   "switch",
-        "for",      "while",
-        "struct",   "extern"
-    };
 
-    //Valid C type identifiers
-    const std::set<std::string> typeIdentifiers{
-        "int", "char", "float", "double", "short", "long", "unsigned"
-    };
+//Valid C keywords
+const std::set<std::string> keywords{
+    "case",     "enum",
+    "register", "typedef",
+    "default",  "goto",
+    "sizeof",   "volatile",
+    "char",     "extern",
+    "return",   "union",
+    "do",       "if",
+    "static",   "switch",
+    "for",      "while",
+    "struct",   "extern"
+};
 
-    //Valid C unary operators
-    const std::set<std::string> unaryOperators{
-        "+", "-", "*", "&", "!", "~"
-    };
+//Valid C type identifiers
+const std::set<std::string> typeIdentifiers{
+    "int", "char", "float", "double", "short", "long", "unsigned"
+};
 
-    //Valid C binary operators
-    const std::set<std::string> binaryOperators{
-        "+", "-", "*", "/", "%", "=", "+=", "-=", "*=", "/=", "%=", // arithmetic
-        "||", "&&", "<", ">", "<=", ">=", "!=", "==",               // logic
-        "|", "&", "^", "<<", ">>", "|=", "&=", "^=", "<<=", ">>=",  // bitwise
-        ".", "->"                                                   // access
-    };
+//Valid C unary operators
+const std::set<std::string> unaryOperators{
+    "+", "-", "*", "&", "!", "~"
+};
 
-    //Valid C ternary operators
-    const std::set<std::string> ternaryOperators{
-        "?", ":"
-    };
+//Valid C binary operators
+const std::set<std::string> binaryOperators{
+    "+", "-", "*", "/", "%", "=", "+=", "-=", "*=", "/=", "%=", // arithmetic
+    "||", "&&", "<", ">", "<=", ">=", "!=", "==",               // logic
+    "|", "&", "^", "<<", ">>", "|=", "&=", "^=", "<<=", ">>=",  // bitwise
+    ".", "->"                                                   // access
+};
 
-    //C special symbols
-    const std::set<std::string> specialSymbols{
-        "{", "}", "[", "]", "(", ")", ";", ","
-    };
+//Valid C ternary operators
+const std::set<std::string> ternaryOperators{
+    "?", ":"
+};
+
+//C special symbols
+const std::set<std::string> specialSymbols{
+    "{", "}", "[", "]", "(", ")", ";", ","
+};
+
 }
