50491b7a4eb8afa56608996351c48cf0f85eb74e
Steven Anaya <steven.anaya@cs.nmt.edu>
2020-02-15 12:13:56 -0700

    Modify test_a.c test file for parser

--- test_files/test_a.c
+++ test_files/test_a.c
@@ -1,6 +1,4 @@
-#define TEST 111
-
-int main(int argc, char **argv) {
+int main() {
     int value = -5;
     float a = 10.1;
     //int 0vaije;
@@ -13,7 +11,7 @@ int main(int argc, char **argv) {
     boi
     */
     char a = '	';
-    for (int i = 0; i < TEST; i++) {
+    for (int i = 0; i < 111; i++) {
         if (i % 10 == 5) {
             value += i;
         }
