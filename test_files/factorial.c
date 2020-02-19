60fe8008d62be195720c3fd3c7f729da4a848149
Steven Anaya <steven.anaya@cs.nmt.edu>
2020-02-18 23:53:38 -0700

    Fix forStmt production and add factorial.c

---
+++ test_files/factorial.c
@@ -0,0 +1,12 @@
+int main() {
+    int fact = 1, n = 10;
+    if (n < 0) {
+        printf("Error! Factorial of a negative number doesn't exist.");
+    } else {
+        for (int i = 1; i <= n; ++i) {
+            fact *= i;
+        }
+        printf("Factorial of %d = %d", n, fact);
+    }
+    return 0;
+}
