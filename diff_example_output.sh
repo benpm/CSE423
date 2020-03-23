2d0407f2dca2ce186bc513ee00d2dc6775c45351
Haydn Jones <haydnjonest@gmail.com>
2020-03-22 19:29:09 -0600

    Diff working

---
+++ diff_example_output.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+# Flags to output examples for
+compilerFlags=(
+    t # Tokens
+    p # Parse tree
+    a # AST
+    s # Symbol Table
+)
+compilerFlagNames=(
+    tokens
+    parseTree
+    AST
+    symbolTable
+)
+
+gitRootDir=$(git rev-parse --show-toplevel)
+examplesDir=$gitRootDir/examples/
+compilerExecutable=$gitRootDir/build/frontend
+
+cd $gitRootDir/build
+
+# Loop over subdirectories in examples
+for dir in $(ls -d $examplesDir*/)
+do
+    cFile=$(find $dir -type f -name "*.c")
+    cFile=${cFile}
+	printf "%s:\n" $(basename -- $cFile)
+
+    # Loop over compiler flags
+    for (( i=0; i<${#compilerFlags[@]}; i++ ));
+    do 
+        flag=${compilerFlags[i]}
+        flagName=${compilerFlagNames[i]}
+        outName=${cFile%".c"}_$flagName.txt
+
+        printf "\tDiffing %s... " $flagName
+
+        # Bash magic to get sterr indented on output
+        $compilerExecutable -l -$flag $cFile > "diff_tmp.txt" 2>&1
+        diff "diff_tmp.txt" $outName > /dev/null
+        if [ "$?" != "0" ]
+        then
+            printf "${RED}FAILED!${NC}\n"
+        else 
+            printf "${GREEN}PASSED!${NC}\n"
+        fi
+    done
+done
