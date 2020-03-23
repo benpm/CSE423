6f605fd93e2b6dd165e6f19ac76d02d9788740f8
Haydn Jones <haydnjonest@gmail.com>
2020-03-23 10:14:42 -0600

    Rebased onto master, updated factorial reference

--- examples/factorial/factorial.c
+++ examples/factorial/factorial.c
@@ -1,12 +1,12 @@
 int main() {
     int fact = 1, n = 10;
     if (n < 0) {
-        return -1; //printf("Error! Factorial of a negative number doesn't exist.");
+        printf("Error! Factorial of a negative number doesn't exist.");
     } else {
         for (int i = 1; i <= n; ++i) {
             fact *= i;
         }
-        return fact; //printf("Factorial of %d = %d", n, fact);
+        printf("Factorial of %d = %d", n, fact);
     }
     return 0;
 }
