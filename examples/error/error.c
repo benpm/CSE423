733f1e4799557bd85e318f4759421d7499309053
Steven Anaya <steven.anaya@cs.nmt.edu>
2020-04-11 04:06:35 -0600

    Remove st_err and add error.c to examples

---
+++ examples/error/error.c
@@ -0,0 +1,22 @@
+// Error: function main undefined
+
+int foo() {
+    int n;
+    int d;
+    int n = 10; // Error: variable redeclared
+    for (int n = 1; n < 10; n++) {
+        d = 5;
+    }
+phoo: // Warning: unused label
+    return n;
+}
+
+// Warning: unused function
+int foo2() {
+    int a;
+    int b = 10, c = 11; // Warning: unused variable c
+    int d = a + b; // Warning: uninitialized variable a
+    goto bar;
+bar:
+    return foo() * d + e; // Error: e not declared
+}
